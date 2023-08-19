6ecb6e082c6d4ebe0de0403c450e725960372090
josdem <joseluis.delacruz@gmail.com>
2023-08-19 08:46:47 -0400

    #1 Deleting database credentials

--- src/main/java/com/jos/dem/springboot/flyway/FlywayDemoApplication.java
+++ src/main/java/com/jos/dem/springboot/flyway/FlywayDemoApplication.java
@@ -2,8 +2,9 @@ package com.jos.dem.springboot.flyway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class FlywayDemoApplication {
 
 	public static void main(String[] args) {
